527db2c0c39911059085255cb40c2f881a1ab919
HayatoKatayama <hayakap1211@gmail.com>
2019-06-30 12:34:08 +0900

    annotate gaze label & concat feature

--- data_analysis/util/feature_extractor.py
+++ data_analysis/util/feature_extractor.py
@@ -1,78 +1,57 @@
 # -*- coding: utf-8 -*-
 """
 全ての特徴量をconcatしたファイルを生成する
+label/
+decode/
+vad/
+gaze/
 """
-from __future__ import print_function
+
 import glob
-import csv
+import pandas as pd
 from argparse import ArgumentParser
 
-class FeatureExtractor(object):
-    def __init__(self, *filenames):
-        self.filenames = list(filenames)
-        self.fi = []
-        self.labels = []
-        for f in self.filenames:
-            self.fi.append(csv.reader(open(f, 'r')))
-        for reader in self.fi:  # ラベルだけ先に抽出
-            self.labels += reader.next()
+def concat_feature(lf, df, vf, gf):
+    df_label = pd.read_csv(lf)
+    df_decode = pd.read_csv(df)
+    df_vad = pd.read_csv(vf)
+    df_gaze = pd.read_csv(gf)
 
-    def __iter__(self):
-        return self
-
-    def next(self):
-        feature = []
-        for reader in self.fi:
-            feature += reader.next()
-        return feature
-
+    assert len(df_label) == len(df_decode), print('cannot concat files.')
+    #print(0,len(df_label))
+    df = pd.concat([df_label, df_decode, df_vad, df_gaze],axis=1)
+    #print(1,len(df))
+    df = df.dropna()
+    #print(2,len(df))
+    return df
 
 if __name__ == '__main__':
     parser = ArgumentParser()
 
-    parser.add_argument("-d", "--datadir", type=str, metavar="DIRECTORY", required=False, default='../../dataset',
+    parser.add_argument("-d", "--datadir", type=str, metavar="DIRECTORY", required=False, default='/Volumes/Untitled/WOZData',
                         help="data directory")
     parser.add_argument("-o", "--outdir", type=str, metavar="DIRECTORY", required=False,
-                        help="output directory", default="../../dataset/feature")
+                        help="output directory", default="/Volumes/Untitled/WOZData/feature")
     parser.add_argument("--debug", required=False,
                         help="is debug", action='store_true')
 
     args = parser.parse_args()
 
-    audio_dir = args.datadir + '/audio'
-    kinect_dir = args.datadir + '/kinect'
-    label_dir = args.datadir + '/label'#_decode'
-    vad_dir = args.datadir + '/vad'
-    cam_dir = args.datadir + '/cam'
+    label_files = sorted(glob.glob(args.datadir + '/label/20171202*.csv'))
+    decode_files = sorted(glob.glob(args.datadir + '/decode/20171202*.csv'))
+    vad_files = sorted(glob.glob(args.datadir + '/vad/20171202*.csv'))
+    gaze_files = sorted(glob.glob(args.datadir + '/gaze/20171202*.csv'))
 
-    audio_files = glob.glob(audio_dir + '/*.csv')
-    kinect_files = glob.glob(kinect_dir + '/*.csv')
-    label_files = glob.glob(label_dir + '/*.csv')
-    vad_files = glob.glob(vad_dir + '/*.csv')
-    cam_files = glob.glob(cam_dir + '/*.csv')
-    print(len(audio_files))
-    #print(audio_files[0].split(".")[-3].split("/")[-1])
-    #print(kinect_files[0].split(".")[-3].split("/")[-1])
-    #print(label_files[0].split(".")[-3].split("/")[-1])
-    #print(vad_files[0].split(".")[-3].split("/")[-1])
-    actions = []
 
-    for lf, vf, af, kf, cf in zip(label_files, vad_files, audio_files, kinect_files, cam_files):
+    for lf, df, vf, gf in zip(label_files, decode_files, vad_files, gaze_files):
         print(lf.split('.')[-3].split('/')[-1])
         if lf.split('.')[-3].split('/')[-1] == vf.split('.')[-3].split('/')[-1] == \
-                af.split('.')[-3].split('/')[-1] == kf.split('.')[-3].split('/')[-1] \
-                 == cf.split('.')[-3].split('/')[-1] :
+                df.split('.')[-3].split('/')[-1] == gf.split('.')[-3].split('/')[-1]:
+
             datetime = lf.split('.')[-3].split('/')[-1]
-            with open("{}/{}.feature.csv".format(args.outdir, datetime), 'w') as fo:
-                extractor = FeatureExtractor(lf, vf, af, kf, cf)
-                print(",".join(extractor.labels), file=fo)
-                for f in extractor:
-                    actions.append(f[0])
-                    print(",".join(f).replace("nan", "0").replace("inf", "0"), file=fo)
+            OUTPUT = "{}/{}.feature.csv".format(args.outdir, datetime)
 
+            concat_df = concat_feature(lf,df,vf,gf)
+            concat_df.to_csv(OUTPUT,index=False)
         else:
-            print("Error")
-
-    from collections import Counter
-    ac_counter = Counter(actions)
-    print(ac_counter)
+            print('error')
--- data_analysis/video/predict_gaze.py
+++ data_analysis/video/predict_gaze.py
@@ -1,35 +1,37 @@
 __author__ = "Hayato Katayama"
 __date__    = "20190304"
-
+import glob
 import cv2, os
 import pandas as pd
 import keras
 
-model = keras.models.load_model('/Users/hayato/shortproject/GazeDetection/keras/gaze_1017_middle50.h5')
+model = keras.models.load_model('/mnt/aoni02/katayama/short_project/proken2018_B/target/gazemodel_128.h5')
 
 
-def predict_gaze(files):
+def predict_gaze(INPUT_FILES):
     """
     @param 1対話分の画像
     return 各画像の予測ラベル(正解ラベル)
     """
     label = []
-    for file in files:
+    for file in INPUT_FILES:
         img = cv2.imread(file, 0).reshape(1, 32, 96, 1)
         prob = model.predict(img / 255.0)
-
-        label.append(np.argmax(prob[0]))
+        prob = 1 if prob[0] > 0.5 else 0
+        label.append(prob)
     return label
 
 
 if __name__ == '__main__':
-    folders = glob.glob('/Users/hayato/Desktop/eye/*')
+    folders = glob.glob('/Volumes/Untitled/WOZData/eye/*')
     print(folders)
     for dir in folders:
         files = sorted(glob.glob(dir + '/*png'))[:]
         label = predict_gaze(files)
         df = pd.DataFrame({'path': files, 'gaze': label})
         path = dir.replace('eye', 'gaze')
-        if not os.path.exists(path):
-            os.mkdir(path)
-        df.to_csv(path + '/a.csv', index=False)
+        #if not os.path.exists(path):
+        #    os.mkdir(path)
+        OUTPUT = path + '.gaze.csv'
+        df.to_csv(OUTPUT, index=False)
+        print('Generated>>',OUTPUT)
